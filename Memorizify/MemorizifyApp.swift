28773f714487bce97524f4724edfbd15e73f2e15
Petr Smejkal <memorizifyapp@gmail.com>
2024-03-16 22:39:03 +0100

    Initial commit

---
+++ Memorizify/MemorizifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizifyApp.swift
+//  Memorizify
+//
+//  Created by Petr Šmejkal on 16.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MemorizifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
