f030f83af60e22eeb81d262423fa9927941f98f0
Petr Smejkal <memorizifyapp@gmail.com>
2024-03-16 21:59:07 +0000

    Merged in IOS-1-project-setup-di-clean-architecture (pull request #1)
    Other: Project Setup (DI, Clean Architecture, etc.)
    
    * Add Resolver dependency; update project structure

---
+++ Memorizify/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Memorizify
+//
+//  Created by Petr Šmejkal on 16.03.2024.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // TODO: Setup Resolver
+        // TODO: Setup Firebase
+        
+        return true
+    }
+}
--- Memorizify/Application/MemorizifyApp.swift
+++ Memorizify/Application/MemorizifyApp.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct MemorizifyApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
